5369d4956d4a69310ce7dfa49d0a8bfe0fb32936
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2023-11-27 00:12:40 +0200

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { SearchbarForm } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { BtnLoadMore } from './Button/Button';
@@ -9,102 +9,90 @@ import css from './App.module.css';
 
 import toast, { Toaster } from 'react-hot-toast';
 
-export class App extends Component {
-  state = {
-    searchValue: '',
-    images: [],
-    page: 1,
-    imgTotal: 0,
-    state: 'init',
-  };
+export const App = () => {
+  const [state, setState] = useState('init');
+  const [searchValue, setSearchValue] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [imgTotal, setImgTotal] = useState(0);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { searchValue, page } = this.state;
-
-    if (prevState.searchValue !== searchValue || prevState.page !== page) {
-      this.axiosGetImg(searchValue, page);
+  useEffect(() => {
+    if (searchValue === '') {
+      return;
     }
-  }
+    axiosGetImg(searchValue, page);
+  }, [searchValue, page]);
 
-  axiosGetImg = async (value, page) => {
+  const axiosGetImg = async (value, page) => {
     try {
-      this.setState({ state: 'pending' });
+      setState('pending');
 
       const { hits, total } = await fetchPhoto(value, page);
 
-      this.setState(prev => ({
-        images: [...prev.images, ...hits],
-        imgTotal: total,
-        state: 'success',
-      }));
+      setImages(prev => [...prev, ...hits]);
+      setImgTotal(total);
+      setState('success');
     } catch (error) {
       toast.error(`Sorry. ${error.message}. Try again.`);
     }
   };
 
-  getImgSubmit = value => {
+  const getImgSubmit = value => {
     if (value === '') {
       toast.error(`Enter the text.`);
       return;
     }
-    this.setState({
-      searchValue: `${Date.now()}/${value}`,
-      images: [],
+    setSearchValue(`${Date.now()}/${value}`);
+    setState('init');
+    setImages([]);
+    setPage(1);
+    setImgTotal(0);
 
-      page: 1,
-      imgTotal: 0,
-      state: 'init',
-    });
     toast.dismiss();
   };
 
-  getImgLoadMore = () => {
-    this.setState(prev => ({ page: prev.page + 1 }));
+  const getImgLoadMore = () => {
+    setPage(prev => prev + 1);
   };
 
-  render() {
-    const { images, imgTotal, searchValue, state, page } = this.state;
-    const { getImgSubmit, getImgLoadMore } = this;
+  return (
+    <div className={css.App}>
+      <SearchbarForm onSubmit={getImgSubmit} />
 
-    return (
-      <div className={css.App}>
-        <SearchbarForm onSubmit={getImgSubmit} />
+      {state === 'success' && <ImageGallery arrImages={images} />}
+      {imgTotal > images.length && <BtnLoadMore onClick={getImgLoadMore} />}
 
-        {state === 'success' && <ImageGallery arrImages={images} />}
-        {imgTotal > images.length && <BtnLoadMore onClick={getImgLoadMore} />}
+      {state === 'pending' && <LoadSpinner />}
+      {searchValue !== '' &&
+        imgTotal > 0 &&
+        images.length === imgTotal &&
+        toast.loading(`No more images`)}
 
-        {state === 'pending' && <LoadSpinner />}
-        {searchValue !== '' &&
-          imgTotal > 0 &&
-          images.length === imgTotal &&
-          toast.loading(`No more images`)}
+      {state === 'success' &&
+        page === 1 &&
+        toast.success(`Find ${imgTotal} images`)}
 
-        {state === 'success' &&
-          page === 1 &&
-          toast.success(`Find ${imgTotal} images`)}
-
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            className: '',
-            duration: 1000,
-            style: {
-              background: '#363636',
-              color: '#fff',
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          className: '',
+          duration: 1000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+          success: {
+            duration: 2000,
+            theme: {
+              primary: 'green',
+              secondary: 'black',
             },
-            success: {
-              duration: 2000,
-              theme: {
-                primary: 'green',
-                secondary: 'black',
-              },
-            },
-            error: {
-              duration: 3000,
-            },
-          }}
-        />
-      </div>
-    );
-  }
-}
+          },
+          error: {
+            duration: 3000,
+          },
+        }}
+      />
+    </div>
+  );
+};
--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,25 +1,19 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import css from './ImageGalleryItem.module.css';
 import { ModalWindow } from 'components/Modal/Modal';
 
-export class CardPhoto extends Component {
-  state = {
-    isOpenModal: false,
-  };
+export const CardPhoto = ({ photo }) => {
+  const { tags, webformatURL } = photo;
+  
+  const [modal, setModal] = useState(false);
 
-  openModal = () => {
-    this.setState({ isOpenModal: true });
+  const openModal = () => {
+    setModal(true);
   };
-  closeModal = () => {
-    this.setState({ isOpenModal: false });
+  
+  const closeModal = () => {
+    setModal(false);
   };
-
-  render() {
-    const { isOpenModal } = this.state;
-    const { openModal, closeModal } = this;
-    const {
-      photo: { tags, webformatURL },
-    } = this.props;
 
     return (
       <>
@@ -28,15 +22,13 @@ export class CardPhoto extends Component {
             className={css['ImageGalleryItem-image']}
             src={webformatURL}
             alt={tags}
-            width="200"
-          />
+            width="200"/>
         </li>
+
         <ModalWindow
           onClose={closeModal}
-          photo={this.props.photo}
-          isOpen={isOpenModal}
-        />
+          photo={photo}
+          isOpen={modal}/>
       </>
     );
-  }
 }
